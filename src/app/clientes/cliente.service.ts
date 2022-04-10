49258ca1d6b3080033ece681adf4598a3e277585
Daniel Rivero <danielcfgm@gmail.com>
2022-04-10 23:59:17 +0200

    added creation form

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
 import {Cliente} from './cliente';
 import {Observable} from 'rxjs';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable(
 )
 export class ClienteService {
 
   private url:string = "http://localhost:8080/api/clientes";
+
+  private headers = new HttpHeaders({'Content-Type':'application/json'});
+
   constructor(private http:HttpClient) { }
 
   getClientes():Observable<Cliente[]>{
     return this.http.get<Cliente[]>(this.url);
   }
+
+  create(cliente:Cliente) : Observable<Cliente>{
+    return this.http.post<Cliente>(this.url, cliente, {headers: this.headers});
+  }
+
 }
